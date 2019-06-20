a7fdd92e26f935d1e8edd05f8d972d3ead3d6930
Noah Fairbairn <noahfairbairn@gmail.com>
2019-06-20 13:32:24 -0700

    added analytics and big fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,40 @@ class Post < ApplicationRecord
   validates :content, presence: true
   validates :title, presence: true
 
+  def self.most_likes
+    @most_likes = 0
+    Post.all.each do |post|
+      if post.post_likes.count > @most_likes
+          @most_likes += post.post_likes.count
+      end
+    end
+    @most_likes
+  end
+
+  def self.total_word_count
+    @total_word_count = 0
+    Post.all.each do |post|
+      words = post.content.split(" ")
+      @total_word_count += words.length
+    end
+    @total_word_count
+  end
+
+  def self.average_word_count
+    @avg = (Post.total_word_count / Post.all.length)
+  end
+
+  def self.total_likes
+    @total = 0
+    Post.all.each do |post|
+      @total += post.post_likes.length
+    end
+    @total
+  end
+
+  def self.avg_likes_per_post
+    @avg = (Post.total_likes / Post.all.length)
+  end
+
+
 end
